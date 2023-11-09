986c6917a6d82d43855311b7a48d022afcb6b7ac
DocDooom <kidsonictrading@gmail.com>
2023-11-09 09:45:17 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('mysql+pymysql://root:123456789@gbdb081123.cmdgrs377mj8.eu-west-2.rds.amazonaws.com:3306/movies'))
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 
 db = SQLAlchemy(app)
